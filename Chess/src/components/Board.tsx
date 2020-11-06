f328e805479b080776cb5923923dd992b6881e37
Shai Gilboa <shizel@gmail.com>
2020-11-06 13:52:06 -0500

    initial board made

--- Chess/src/components/Board.tsx
+++ Chess/src/components/Board.tsx
@@ -1,4 +1,4 @@
-import React, { PropsWithChildren, ReactElement } from 'react';
+import React, { PropsWithChildren, ReactElement, useEffect, useState } from 'react';
 import styled from '@emotion/styled';
 import Square from './Square';
 import Knight from './Kight';
@@ -10,25 +10,37 @@ interface props {
 };
 
 const Board : React.FC<PropsWithChildren<props>> = ({knightPosition = [1,0]}) => {
+  const [squares, setSquares] = useState<ReactElement[]>([]);
 
   const renderSquare = (index : number, [knightX, knightY] : number[]) : ReactElement  => {
-    const row : number = index % 8
-    const column : number = Math.floor(index / 8)
+    const column : number = index % 8
+    const row : number = Math.floor(index / 8)
     const color : "white" | "black" = (row + column) % 2 === 1 ? "black" : "white";
-    const isKnight : boolean =  (knightX === row && knightY === column);
-
+    const isKnight : boolean =  (knightX === column && knightY === row);
+    console.log('row, column', row, column)
+    console.log('isKnight', isKnight)
     return (
       <SquareWrapper key={`square[${row},${column}]`}>
         <Square color={color}>{isKnight ? <Knight /> : null}</Square>
-      </SquareWrapper>)
+      </SquareWrapper>
+      )
   }
 
-  const arr = [];
-  arr[63] = 1;
-  console.log('arr', arr)
+  useEffect(()=>{
+    const newSquares : ReactElement[] = [];
+    for (let index = 0; index < 64; index ++) {
+      // console.log('squares in useEffect', squares.length)
+      // let newSquares = squares.concat(renderSquare(index, knightPosition))
+      newSquares.push(renderSquare(index, knightPosition))
+    }
+    // setSquares([renderSquare(0,knightPosition),renderSquare(1,knightPosition),renderSquare(2,knightPosition),renderSquare(3,knightPosition)])
+    setSquares(newSquares)
+  },[])
+
+  console.log('squares', squares)
   return (
     <Wrapper data-css='Board'>
-      {arr.map((x : any, index : number) => renderSquare(index, knightPosition))}
+      {squares}
     </Wrapper>
   )
 }
@@ -38,6 +50,8 @@ export default Board;
 const Wrapper = styled.div`
   width: 100%;
   height: 100%;
+  display: flex;
+  flex-wrap: wrap;
 `;
 
 const SquareWrapper = styled.div`
